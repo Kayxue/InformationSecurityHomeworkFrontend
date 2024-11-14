639f62d52d0b0149a46b54d0bbb21addd6b3822c
Kayxue <s0990006@gmail.com>
2024-11-14 13:53:51 +0800

    Formatting

--- src/app/fetcher.ts
+++ src/app/fetcher.ts
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-export const fetcher = (url: string) => axios.get(url,{withCredentials:true}).then((res) => res.data);
+export const fetcher = (url: string) =>
+	axios.get(url, { withCredentials: true }).then((res) => res.data);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,15 +19,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
